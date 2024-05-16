84be8bd202d05f9375d09a7237fafec7e95cbb82
huyixi <hi@huyixi.com>
2024-05-16 23:23:16 +0800

    feat: add banner

---
+++ src/data/banner.config.ts
@@ -0,0 +1,9 @@
+// banner.config.ts
+import springUpdatesImage from '@/assets/images/spring_updates.webp'
+
+export const bannerConfig = {
+	title: 'Spring Update',
+	description: 'Introducing GPT-40 and making more capabilities available for free in ChatGPT.',
+	image: springUpdatesImage,
+	url: '/post/astro'
+}
